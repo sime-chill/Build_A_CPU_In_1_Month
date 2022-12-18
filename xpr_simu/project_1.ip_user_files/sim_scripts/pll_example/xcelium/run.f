cc4526bf755b7f0c6154916c7280bc3cdc801097
yuhan <heyh39@mail2.sysu.edu.cn>
2022-12-18 20:58:43 +0800

    add xpr_simu

---
+++ xpr_simu/project_1.ip_user_files/sim_scripts/pll_example/xcelium/run.f
@@ -0,0 +1,15 @@
+-makelib xcelium_lib/xpm -sv \
+  "F:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "F:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "F:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../project_1.srcs/sources_1/ip/pll_example/pll_example_clk_wiz.v" \
+  "../../../../project_1.srcs/sources_1/ip/pll_example/pll_example.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
